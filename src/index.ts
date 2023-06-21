6f1e852ba93beba0e6b2ff7da87284a99036348d
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2023-06-21 13:16:09 +0300

    start_server

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,21 @@ const app: Application = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
+//тут просто от себя для разных окружений
 dotenv.config({
 
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 })
 
-// я опишу интерфйес для юсеров прямо тут для удобства проверки, файл будет не большой все равно
+// я опишу интерфйес для юсеров прямо тут для удобства проверки, файл будет небольшой все равно
 // но естественно все в отдельном фале должно быть
 interface User {
 
     id: string
     email: string
     hashedPassword: string
+    number?: string
 }
 
 //я добавлю массив юсеров здесь по той же причине
@@ -59,3 +62,46 @@ const users: User[] = [
       hashedPassword: bcrypt.hashSync('822287', 8),
     }
   ]
+
+//здесь получаем этих юсеров
+
+const getUsers = (query: any): User[] => {
+
+    let searchUsers: User[] = []
+
+    if (query.email) {
+//вот тут передали и сконкатели
+        searchUsers = searchUsers.concat(
+
+            users.filter((user) => user.email === query.email)
+        )
+    }
+
+    if (query.number) {
+
+        searchUsers = searchUsers.concat(
+
+            users.filter((user) => user.number === query.number)
+        )
+    }
+
+    return searchUsers
+}
+//тут запросим
+//я бы лучше писал эвейтом, но тут один запрос и ниже можно сделать второй на ошибку, так что просто так
+app.get('/users', (req: Request, res: Response) => {
+
+    const searchUsers = getUsers(req.query)
+
+    //тут задержка по заданию
+    setTimeout(() => {
+
+        res.status(200).send(searchUsers)
+    })
+})
+
+
+const server = app.listen(process.env.PORT || 3000, () => {
+
+    console.log(`Pasha start server`)
+  })
